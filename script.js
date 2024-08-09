a495958c426ca7324f8aa56d3509d5416b65c11c
Rangerbearr <eli.orille@yahoo.com>
2024-08-09 10:44:08 -0500

    Completed game logic for the array

--- script.js
+++ script.js
@@ -7,9 +7,54 @@ function player(name){
     return{name};
 }
 
+function fillBoard(){
+    
+}
+
+function checkWin(gb){
+    if(gb.board[0] == gb.board[1] && gb.board[1] == gb.board[2] && gb.board[0] != undefined){
+        console.log('win');
+        endGame();
+    }
+    if(gb.board[3] == gb.board[4] && gb.board[4] == gb.board[5] && gb.board[3] != undefined){
+        console.log('win');
+        endGame();
+    }
+    if(gb.board[7] == gb.board[8] && gb.board[8] == gb.board[9] && gb.board[7] != undefined){
+        console.log('win');
+        endGame();
+    }
+    if(gb.board[0] == gb.board[3] && gb.board[3] == gb.board[6] && gb.board[0] != undefined){
+        console.log('win');
+        endGame();
+    }
+    if(gb.board[1] == gb.board[4] && gb.board[4] == gb.board[7] && gb.board[1] != undefined){
+        console.log('win');
+        endGame();
+    }
+    if(gb.board[2] == gb.board[5] && gb.board[5] == gb.board[8] && gb.board[2] != undefined){
+        console.log('win');
+        endGame();
+    }
+    if(gb.board[0] == gb.board[4] && gb.board[4] == gb.board[8] && gb.board[0] != undefined){
+        console.log('win');
+        endGame();
+    }
+    if(gb.board[2] == gb.board[4] && gb.board[4] == gb.board[6] && gb.board[2] != undefined){
+        console.log('win');
+        endGame();
+    }
+}
+
+function endGame(){
+    const submitButton = document.querySelector('#submitButton');
+    const newButton = submitButton.cloneNode(true);
+    submitButton.parentNode.replaceChild(newButton, submitButton);
+
+}
+
 function createGame(gb,pOne,pTwo){
     const submitButton = document.querySelector('#submitButton');
-    let turn = 0;
     submitButton.addEventListener('click', ()=>{
         const pOnepick = document.getElementById('pick').value;
         if( (-1 < pOnepick && pOnepick < 9) && gb.board[pOnepick] == undefined){
@@ -20,9 +65,7 @@ function createGame(gb,pOne,pTwo){
             }
             turn++;
             console.log(gb.board);
-            if(gb.board[0] == gb.board[1] && gb.board[1] == gb.board[2]){
-                console.log('win');
-            }
+            checkWin(gb);
         }
     },);
 }
